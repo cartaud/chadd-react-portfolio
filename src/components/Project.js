c3f0992c12a921afa54bd88a70ef4ab11d5308de
Chadd <chadd_artaud@cox.net>
2022-08-17 15:47:41 -0700

    improved README

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,10 +9,12 @@ const styles = {
         display: 'flex',
     },
     fit: {
-        height: '100%'
+        height: '0',
+        maxHeight: 'fit-content',
     },
     img: {
-        maxWidth: '90%',
+        maxWidth: '85%',
+        maxHeight: 'fit-content',
         border: '2px #f19b2c solid',
         transition: 'all 0.5s ease-in-out',
     },
@@ -26,10 +28,10 @@ const styles = {
         textDecoration: 'none',
         color: 'white',
         padding: '1% 2%',
-        zIndex: 1,
+        zIndex: '1',
     },
     header: {
-        margin: 0,
+        margin: '0',
     },
 };
 
@@ -46,29 +48,29 @@ const Project = () => { //create a project object and map over it instead of rep
                 <div className="project-container">
                     <a href="https://github.com/cartaud/mvcTechBlog" target="_blank" rel="noreferrer" style={styles.fit}>
                     <img src={require('../images/techBlog.png')}  alt="Tech Blog" style={styles.img}/>
+                    </a>
                     <div style={styles.title}>
                         <h5 style={styles.header}>Tech Blog</h5>
                         <h6 style={styles.header}>MVC</h6>
                     </div>
-                    </a>
                 </div>
                 <div className="project-container">
                     <a href="https://github.com/cartaud/E-CommerceBackEnd" target="_blank" rel="noreferrer" style={styles.fit}>
                     <img src={require('../images/eCom.png')}  alt="E-Commerce" style={styles.img}/>
+                    </a>
                     <div style={styles.title}>
                         <h5 style={styles.header}>E-Commerce</h5>
                         <h6 style={styles.header}>ORM</h6>
                     </div>
-                    </a>
                 </div>
                 <div className="project-container">
                     <a href="https://github.com/cartaud/expressNoteTaker" target="_blank" rel="noreferrer" style={styles.fit}>
                     <img src={require('../images/noteTaker.png')}  alt="Note Taker" style={styles.img}/>
+                    </a>
                     <div style={styles.title}>
                         <h5 style={styles.header}>Note Taker</h5>
                         <h6 style={styles.header}>Express.JS</h6>
                     </div>
-                    </a>
                 </div>
             </div>
 
@@ -76,29 +78,29 @@ const Project = () => { //create a project object and map over it instead of rep
                 <div className="project-container">
                     <a href="https://github.com/cartaud/employeeTracker" target="_blank" rel="noreferrer" style={styles.fit}>
                     <img src={require('../images/employeeTracker.png')}  alt="Employee Tracker" style={styles.img}/>
+                    </a>
                     <div style={styles.title}>
                         <h5 style={styles.header}>Employee Tracker</h5>
                         <h6 style={styles.header}>SQL</h6>
                     </div>
-                    </a>
                 </div>
                 <div className="project-container">
                     <a href="https://cartaud.github.io/etch-a-sketch/" target="_blank" rel="noreferrer" style={styles.fit}>
                     <img src={require('../images/etch.png')}  alt="Etch-A-Sketch" style={styles.img}/>
+                    </a>
                     <div style={styles.title}>
                         <h5 style={styles.header}>Etch-A-Sketch</h5>
                         <h6 style={styles.header}>Javascript</h6>
                     </div>
-                    </a>
                 </div>
                 <div className="project-container">
                     <a href="https://github.com/cartaud/vinylMusicShop" target="_blank" rel="noreferrer" style={styles.fit}>
                     <img src={require('../images/musicStore.png')}  alt="Music Store" style={styles.img}/>
+                    </a>
                     <div style={styles.title}>
                         <h5 style={styles.header}>Music Store</h5>
                         <h6 style={styles.header}>Fullstack</h6>
                     </div>
-                    </a>
                 </div>
             </div>
         </div>
